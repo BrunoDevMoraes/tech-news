e7f431492df0dea4822aee245f754c71b2c5cf0c
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-08-30 18:46:12 -0300

    add 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
@@ -9,7 +10,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    except ValueError:
+        raise ValueError('Data inválida')
+    else:
+        year = date.split('-')[0]
+        month = date.split('-')[1]
+        day = date.split('-')[2]
+        new_date = f"{day}/{month}/{year}"
+        query = {"timestamp": new_date}
+        return [(news["title"], news["url"]) for news in search_news(query)]
 
 
 # Requisito 8
